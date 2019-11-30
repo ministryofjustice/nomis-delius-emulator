aa405187a6d1bbc74bf9d3821518bafbd51a37f6
Stephen Dicks <stephen@starswan.com>
2019-11-30 09:05:46 +0000

    add offender model

---
+++ app/models/offender.rb
@@ -0,0 +1,2 @@
+class Offender < ApplicationRecord
+end
---
+++ db/migrate/20191130090429_create_offenders.rb
@@ -0,0 +1,17 @@
+class CreateOffenders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :offenders do |t|
+      t.string :gender, limit: 1
+      t.string :mainOffence
+      t.date :receptionDate
+      t.string :firstName
+      t.string :lastName
+      t.string :offenderNo
+      t.string :convictedStatus
+      t.string :imprisonmentStatus
+      t.date :dateOfBirth
+
+      t.timestamps
+    end
+  end
+end
