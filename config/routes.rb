8b8f4c6a5909c852b99ac6d144679d1a633e0c84
Stephen Dicks <stephen@starswan.com>
2019-12-02 20:55:07 +0000

    add default route as active admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
+  root 'admin/dashboard#index'
+
   get "/elite2api/api/locations/description/:prison_id/inmates" => 'offenders#index'
   post '/elite2api/api/offender-sentences/bookings' => 'bookings#index'
 end
