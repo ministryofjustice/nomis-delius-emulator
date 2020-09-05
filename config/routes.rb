ddc9f4ad85e5d758aaabb106249968db0b409d72
Stephen Dicks <stephen@starswan.com>
2020-09-05 20:03:24 +0100

    removed elite2 from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'admin/dashboard#index'
 
-  get "/elite2api/api/locations/description/:prison_id/inmates" => 'offenders#index'
-  post '/elite2api/api/offender-sentences/bookings' => 'bookings#index'
+  get "/api/locations/description/:prison_id/inmates" => 'offenders#index'
+  post '/api/offender-sentences/bookings' => 'bookings#index'
 end
