092ece7f422ed9813555a8095bc8fea2deb58dea
Stephen Dicks <stephen@starswan.com>
2020-10-20 20:49:03 +0100

    appease linter

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,6 +1,6 @@
 class Movement < ApplicationRecord
-  belongs_to :from_prison, class_name: Prison.name, optional: true
-  belongs_to :to_prison, class_name: Prison.name, optional: true
+  belongs_to :from_prison, class_name: "Prison", optional: true
+  belongs_to :to_prison, class_name: "Prison", optional: true
 
   validates :typecode, inclusion: { in: %w[ADM TAP REL TRN], allow_nil: false }
 end
