b60f7d976871056b1a219efe3e974655c604dcd4
Stephen Dicks <stephen@starswan.com>
2021-02-01 11:51:06 +0000

    removed controller test, as movementTypes parameter can't be passed using Rack::Test

--- app/controllers/nomis/api/movements_controller.rb
+++ app/controllers/nomis/api/movements_controller.rb
@@ -9,12 +9,8 @@ module Nomis
       skip_before_action :verify_authenticity_token, only: :index
 
       def index
-        if request.query_string.blank?
-          types = params[:movementTypes]
-        else
-          raw = request.query_string.split("&")
-          types = raw.select { |h| h.starts_with?("movementTypes=") }.map { |t| t.split("=").second }
-        end
+        raw = request.query_string.split("&")
+        types = raw.select { |h| h.starts_with?("movementTypes=") }.map { |t| t.split("=").second }
         offender_ids = JSON.parse(request.body.string)
         @movements = Movement.includes(:offender, :to_prison, :from_prison).
           by_type(types).joins(:offender).merge(Offender.by_offender_no(offender_ids))
