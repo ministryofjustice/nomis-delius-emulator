cc44208c1c43bed15efc381ff5bbe7bef0e022b1
Stephen Dicks <stephen@starswan.com>
2021-02-01 11:51:06 +0000

    removed controller test, as movementTypes parameter can't be passed using Rack::Test

--- spec/controllers/nomis/api/movements_controller_spec.rb
+++ spec/controllers/nomis/api/movements_controller_spec.rb
@@ -35,42 +35,4 @@ RSpec.describe Nomis::Api::MovementsController, type: :controller do
                                                }].map(&:stringify_keys))
     end
   end
-
-  describe "GET #index" do
-    it "returns http success" do
-      post :index, params: { latestOnly: true, movementTypes: %w[ADM TRN] }, body: [offender.offenderNo].to_json, format: :json
-      expect(response).to have_http_status(:success)
-
-      expect(JSON.parse(response.body)).
-          to eq([{
-                     toAgency: prison.code,
-                     createDateTime: JSON.parse(offender.movements.first.created_at.to_json),
-                     movementType: "ADM",
-                     directionCode: "IN",
-                     offenderNo: offender.offenderNo,
-                 },
-                 {
-                     fromAgency: prison.code,
-                     toAgency: prison2.code,
-                     createDateTime: JSON.parse(movement.created_at.to_json),
-                     movementType: "TRN",
-                     directionCode: "IN",
-                     offenderNo: offender.offenderNo,
-                 }].map(&:stringify_keys))
-    end
-
-    it "filters by type" do
-      post :index, params: { latestOnly: true, movementTypes: %w[ADM] }, body: [offender.offenderNo].to_json, format: :json
-      expect(response).to have_http_status(:success)
-
-      expect(JSON.parse(response.body)).
-          to eq([{
-                     toAgency: prison.code,
-                     createDateTime: JSON.parse(offender.movements.first.created_at.to_json),
-                     movementType: "ADM",
-                     directionCode: "IN",
-                     offenderNo: offender.offenderNo,
-                 }].map(&:stringify_keys))
-    end
-  end
 end
