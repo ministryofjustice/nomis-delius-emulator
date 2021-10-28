18eaa8f521e5856dc7dc9c878e526ed848783d48
Ollie Treend <ollie@treend.com>
2021-10-28 16:26:27 +0100

    Add validation for NOMIS offender IDs
    Offender IDs are validated in MPC now, so invalid IDs in this emulator
    will cause MPC to crash.
    
    Therefore, I've introduced the same validation rule here to be a bit
    more helpful to users when adding new offenders.

--- spec/models/offender_spec.rb
+++ spec/models/offender_spec.rb
@@ -69,4 +69,31 @@ RSpec.describe Offender, type: :model do
       it { is_expected.to eq "RECALL" }
     end
   end
+
+  describe "#offenderNo" do
+    # NOMIS offender IDs must be of the form <letter><4 numbers><2 letters>
+    let(:valid_ids) { %w[A0000AA Z5432HD A4567CD] }
+
+    let(:invalid_ids) do
+      [
+        "A 1234 AA", # no spaces allowed
+        "E123456", # this is a nDelius CRN, not a NOMIS ID
+        "A0000aA", # must be all uppercase
+        "", # cannot be empty
+        nil, # cannot be nil
+        "1234567",
+        "ABCDEFG",
+      ]
+    end
+
+    it "requires a valid NOMIS offender ID" do
+      valid_ids.each do |id|
+        expect(build(:offender, offenderNo: id)).to be_valid
+      end
+
+      invalid_ids.each do |id|
+        expect(build(:offender, offenderNo: id)).not_to be_valid
+      end
+    end
+  end
 end
