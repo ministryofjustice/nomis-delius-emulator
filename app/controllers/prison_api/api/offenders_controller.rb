03d2eb4e2dccd319ded9eb3ad25524a33b73507c
Ollie Treend <ollie@treend.com>
2021-10-28 15:04:21 +0100

    Remove unused Prison API endpoints
    The Prison API endpoints to retrieve offender records and bookings are no longer needed following MO-806 because they're read from the Search API instead.

--- app/controllers/prison_api/api/offenders_controller.rb
+++ app/controllers/prison_api/api/offenders_controller.rb
@@ -8,17 +8,6 @@ module PrisonApi
       # This is a POST action but is just a hidden GET really
       skip_before_action :verify_authenticity_token, only: :assessments
 
-      def index
-        offset = request.headers["Page-Offset"]
-        limit = request.headers["Page-Limit"]
-        prison = Prison.find_by(code: params[:prison_id])
-        @offenders = prison.offenders.order(:offenderNo).joins(:booking).offset(offset).limit(limit)
-      end
-
-      def show
-        @offender = Offender.find_by(offenderNo: params[:offender_no])
-      end
-
       def assessments
         offender_ids = JSON.parse(request.body.string)
         @offenders = Offender.where(offenderNo: offender_ids)
